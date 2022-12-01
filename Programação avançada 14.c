74b48382e18bab1173e7d8a198caf9371731f2d9
Henrique Vilas Boas <89943034+Rick97h@users.noreply.github.com>
2022-12-01 09:12:54 -0300

    Create Programação avançada 14.c

---
+++ Programação avançada 14.c
@@ -0,0 +1,24 @@
+10) Imprimir um quadrado de esteriscos.
+
+void imprimeRetangulo (int n) 
+{
+    for (int l=1; l<=n; l++)
+    {
+        for (int c=1; c<=n * 5; c++)
+            printf ("*");
+            
+        printf ("\n");
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Qual o tamanho desejado: \n");
+    scanf("%d", &n);
+    printf("Quadrado de %d\n", n);
+    
+    imprimeRetangulo(n);
+    
+    return(0);
+}
