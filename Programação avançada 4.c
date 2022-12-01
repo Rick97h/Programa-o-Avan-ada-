1e77ca7c125c13de6b4ff1137087c5cb74cf41c1
Henrique Vilas Boas <89943034+Rick97h@users.noreply.github.com>
2022-12-01 09:19:35 -0300

    Update Programação avançada 4.c

--- Programação avançada 4.c
+++ Programação avançada 4.c
@@ -1,11 +1,11 @@
-/******************************************************************************
-Nome: Rafael Lima Morial
-*******************************************************************************/
+
 #include <stdio.h>
 
 int main()
 {
+    //variaveis:
     float a = 7, b = 2;
+   //saida:
     printf("A soma de %.2f e %.2f é %.2f \n", a, b, a + b);
     printf("A subtração é %.2f \n", a - b);
     printf("A divisao é %.2f \n", a / b);
