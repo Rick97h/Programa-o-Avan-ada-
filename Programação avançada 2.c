527c7ab8e693f1508442e106fb15c07518f75798
Henrique Vilas Boas <89943034+Rick97h@users.noreply.github.com>
2022-12-01 09:00:17 -0300

    Create Programação avançada 2.c

---
+++ Programação avançada 2.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+Nome:Rafael Lima Morial ADS
+*******************************************************************************/
+#include <stdio.h>
+
+1) Informar em qual das três posições colocadas está o maior número.
+
+int main()
+{
+    // variaveis
+    int a,b,c,posi, maior;
+    
+    // entrada
+    printf("digite 3 numeros:");
+    scanf("%d %d %d", &a,&b,&c);
+    
+    // processamento
+    maior=a; posi=1;
+    if (b>maior) {maior=b; posi=2;}
+    if (c>maior) {maior=c; posi=3;}
+    
+    //saida
+    printf("maior esta na posicao %d", posi);
+
+    return 0;
+}
