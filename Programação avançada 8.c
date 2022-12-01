159f9d27f17d7574cb6751f2436e688f03c4ef80
Henrique Vilas Boas <89943034+Rick97h@users.noreply.github.com>
2022-12-01 09:05:49 -0300

    Create Programação avançada 8.c

---
+++ Programação avançada 8.c
@@ -0,0 +1,18 @@
+4) Ler 6 vetores inteiros e imprimir os valores lidos de forma inversa.
+
+int main() {
+   int v[TAMANHO];
+   
+   printf ("(ENTRADA) Digite %d valores\n", TAMANHO);
+   for (int i=0; i< TAMANHO ;i++) {
+       printf ("v[%d]: ", i);
+       scanf ("%d", &v[i]);
+   }
+   
+   printf ("\n(SAÍDA) Em ordem inversa:");
+   for (int i= TAMANHO-1 ; i>=0 ; i--) {
+        printf ("\nv[%d]: %d", i,v[i]);
+   }
+
+    return 0;
+}
