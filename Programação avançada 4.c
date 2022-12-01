54bb73aeeaf22e9a2b901da4a60954007f958485
Henrique Vilas Boas <89943034+Rick97h@users.noreply.github.com>
2022-12-01 09:02:26 -0300

    Create Programação avançada 4.c

---
+++ Programação avançada 4.c
@@ -0,0 +1,15 @@
+/******************************************************************************
+Nome: Rafael Lima Morial
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    float a = 7, b = 2;
+    printf("A soma de %.2f e %.2f é %.2f \n", a, b, a + b);
+    printf("A subtração é %.2f \n", a - b);
+    printf("A divisao é %.2f \n", a / b);
+    printf("A multiplicação é %.2f \n", a * b);
+
+    return 0;
+}
