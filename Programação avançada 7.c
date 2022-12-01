a93ebd280ec7587ba36499f180987426be5e631d
Henrique Vilas Boas <89943034+Rick97h@users.noreply.github.com>
2022-12-01 09:21:09 -0300

    Update Programação avançada 7.c

--- Programação avançada 7.c
+++ Programação avançada 7.c
@@ -1,4 +1,4 @@
-3) Contar e escrever quantos valores pares as 10 posições do vetor tem.
+//Contar e escrever quantos valores pares as 10 posições do vetor tem.
 
 int main() {
    int v[TAMANHO], totalPares=0;
@@ -12,7 +12,7 @@ int main() {
    for (int i=0; i< TAMANHO ;i++) {
        if (v[i] %2 == 0) totalPares++;
    }
-
+//saida
    printf ("\n(SAIDA) A quantidade de pares é %d", totalPares);
 
     return 0;
