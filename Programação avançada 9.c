a5edf7ccfd8c124c32b271cad70f936a2cd969a0
Henrique Vilas Boas <89943034+Rick97h@users.noreply.github.com>
2022-12-01 09:06:26 -0300

    Create Programação avançada 9.c

---
+++ Programação avançada 9.c
@@ -0,0 +1,19 @@
+5) Imprimir um retângulo de triângulos.
+
+void imprimeRetangulo (int lin, int col) {
+    for (int l=1; l<=lin; l++){
+        for (int c=1; c<=col; c++)
+            printf ("*");
+        printf ("\n");
+    }
+}
+int main()
+{
+    int l, c;
+    printf ("Linhas? ");
+    scanf ("%d", &l);
+    printf ("Colunas? ");
+    scanf ("%d", &c);
+    imprimeRetangulo(l,c); 
+    return 0;
+}
