1d0a1350b1940193ed3ba9db8f79e95b3bcadc4b
Henrique Vilas Boas <89943034+Rick97h@users.noreply.github.com>
2022-12-01 09:04:01 -0300

    Create Programação avançada 6.c

---
+++ Programação avançada 6.c
@@ -0,0 +1,26 @@
+2) Saber o salário total de um funcionário:
+
+int main()
+{
+    // Váriaveis
+    float salFixo, vendas, salTotal;
+    
+    //entrada
+    printf("Digite o seu salário fixo: ");
+    scanf("%f", &salFixo);
+    printf("Digite o valor das vendas: ");
+    scanf("%f", &vendas);
+    
+    //processamento
+    if (vendas > 1500)
+     salTotal = 1500 * 3 / 100 + (vendas - 1500) * 5 / 100;
+   
+    else
+    salTotal = 1500 * 3 / 100;
+    
+    //Saída
+    salTotal = salTotal + salFixo;
+    printf("%0.3f", salTotal);
+
+    return 0;
+}
